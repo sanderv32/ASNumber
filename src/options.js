bc223b703986465215f834326800451d6c8bcfaa
Alexander Verhaar <sanderv32@gmail.com>
2015-10-18 14:40:36 +0200

    Changed tabs to spaces

--- src/options.js
+++ src/options.js
@@ -1,55 +1,55 @@
 function restore_options() {
-	var ttl = localStorage["ttl"];
-	var rest_dns = localStorage["rest_dns"];
-	if (!ttl) ttl = default_ttl;
-	if (!rest_dns) rest_dns = default_rest_dns;
-	document.getElementById("ttl").value = ttl/1000;
-	document.getElementById("restDNS").value = rest_dns;
+        var ttl = localStorage["ttl"];
+        var rest_dns = localStorage["rest_dns"];
+        if (!ttl) ttl = default_ttl;
+        if (!rest_dns) rest_dns = default_rest_dns;
+        document.getElementById("ttl").value = ttl/1000;
+        document.getElementById("restDNS").value = rest_dns;
 
-	HumanReadable();
+        HumanReadable();
 }
 
 function save_options() {
-	var status = document.getElementById("status");
-	localStorage["ttl"] = document.getElementById("ttl").value*1000;
-	localStorage["rest_dns"] = document.getElementById("restDNS").value;
-	status.innerHTML = "Options saved...";
-	setTimeout(function() {
-			status.innerHTML = "";
-			}, 750);
+        var status = document.getElementById("status");
+        localStorage["ttl"] = document.getElementById("ttl").value*1000;
+        localStorage["rest_dns"] = document.getElementById("restDNS").value;
+        status.innerHTML = "Options saved...";
+        setTimeout(function() {
+                        status.innerHTML = "";
+                        }, 750);
 }
 
 function HumanReadable() {
-	var ttl = document.getElementById("ttl").value;
-	var hr = document.getElementById("hr");
-	var tmptxt = "";
+        var ttl = document.getElementById("ttl").value;
+        var hr = document.getElementById("hr");
+        var tmptxt = "";
 
-	var seconds = ttl%60;
-	var minutes = Math.round(ttl/60%60);
-	var hours   = Math.round(ttl/3600%24);
-	var days    = Math.floor(ttl/86400);
+        var seconds = ttl%60;
+        var minutes = Math.round(ttl/60%60);
+        var hours   = Math.round(ttl/3600%24);
+        var days    = Math.floor(ttl/86400);
 
-	tmptxt=days+" day";
-	if ( days > 1 )
-		tmptxt=tmptxt+"s, ";
-	else 
-		tmptxt=tmptxt+", ";
+        tmptxt=days+" day";
+        if ( days > 1 )
+                tmptxt=tmptxt+"s, ";
+        else 
+                tmptxt=tmptxt+", ";
 
-	tmptxt=tmptxt+hours+" hours, "+minutes+" minutes, "+seconds+" seconds";
+        tmptxt=tmptxt+hours+" hours, "+minutes+" minutes, "+seconds+" seconds";
 
-	hr.innerHTML=tmptxt;
+        hr.innerHTML=tmptxt;
 
 }
 
 
 document.addEventListener('DOMContentLoaded', function() {
-	restore_options();
+        restore_options();
 
-	document.getElementById('ttl').addEventListener('change', function() {
-		HumanReadable();
-	}, false);
+        document.getElementById('ttl').addEventListener('change', function() {
+                HumanReadable();
+        }, false);
 
-	document.getElementById('saveButton').addEventListener('click', function() {
-		save_options();
-	});
+        document.getElementById('saveButton').addEventListener('click', function() {
+                save_options();
+        });
 });
--- src/popup.js
+++ src/popup.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
-	var manifest = chrome.runtime.getManifest();
-	var version = document.getElementById("version");
-	version.innerHTML = "Version: "+manifest.version;
-	chrome.runtime.sendMessage({ command: "selected-tab" });
+        var manifest = chrome.runtime.getManifest();
+        var version = document.getElementById("version");
+        version.innerHTML = "Version: "+manifest.version;
+        chrome.runtime.sendMessage({ command: "selected-tab" });
 });
