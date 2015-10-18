3183f37986a8e961eb26cce910c03b917b642adb
Alexander Verhaar <sanderv32@gmail.com>
2015-10-18 14:39:24 +0200

    Changed caching class

--- src/cacheClass.js
+++ src/cacheClass.js
@@ -1,59 +1,61 @@
 /*
- * Very simple cachine class
+ * Very simple caching class
  */
-function Cache() {
-	this.entry = {};
-	this.ttl = ( 8 * 60 * 60 * 1000 );
-}
-Cache.prototype.Add = function(k,o) {
-	var newEntry = {
-		data: {},
-		ttl: {}
-	};
-	newEntry.data = o;
-	newEntry.ttl = new Date().getTime()+this.ttl;
-	this.entry[k] = newEntry;
-}
-Cache.prototype.Get = function(k) {
-	var d = new Date();
-	if (this.entry[k]) {
-		if (this.entry[k].ttl < d.getTime()) {
-			/* Entry expired, delete it */
-			delete this.entry[k];
-			return null;
-		} else {
-			/* Entry not expired, but queried within ttl time
-			 * update ttl time again
-			 */
-			this.entry[k].ttl = d.getTime()+this.ttl;
-		}
-	}
-	return this.entry[k];
-}
-Cache.prototype.Remove = function(k) {
-	delete this.entry[k];
-}
-Cache.prototype.hasKey = function(k) {
-	var d = new Date();
-	var result = false;
+var Cache = function() {
+        this.entry = {};
+        this.ttl = ( 8 * 60 * 60 * 1000 );
 
-	if (this.entry[k]) {
-		result = true;
-	} else {
-		result=false;
-	}
+        this.Add = function(k,o) {
+                var newEntry = {
+                        data: {},
+                        ttl: {}
+                };
+                newEntry.data = o;
+                newEntry.ttl = new Date().getTime()+this.ttl;
+                this.entry[k] = newEntry;
+        }
+        this.Get = function(k) {
+                var d = new Date();
+                if (this.entry[k]) {
+                        if (this.entry[k].ttl < d.getTime()) {
+                                /* Entry expired, delete it */
+                                delete this.entry[k];
+                                return null;
+                        } else {
+                                /* Entry not expired, but queried within ttl time
+                                 * update ttl time again
+                                 */
+                                this.entry[k].ttl = d.getTime()+this.ttl;
+                        }
+                }
+                return this.entry[k];
+        }
+        this.Remove = function(k) {
+                delete this.entry[k];
+        }
+        this.hasKey = function(k) {
+                var d = new Date();
+                var result = false;
 
-	if (result==true && this.entry[k].ttl < d.getTime()) {
-		delete this.entry[k];
-		result=false;
-	}
+                if (this.entry[k]) {
+                        result = true;
+                } else {
+                        result=false;
+                }
 
-	return result;
+                if (result==true && this.entry[k].ttl < d.getTime()) {
+                        delete this.entry[k];
+                        result=false;
+                }
+
+                return result;
+        }
+        this.length = function() {
+                var l=0;
+                for(var i in this.entry) {
+                        l++;
+                }
+                return l;
+        }
 }
-Cache.prototype.length = function() {
-	var l=0;
-	for(var i in this.entry) {
-		l++;
-	}
-	return l;
-}
+/* vim: set ts=8 sw=8 tw=0 ft=javascript noet : */
